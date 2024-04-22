fec8e61fc24522e5a2ae2370e47eabbd849bef24
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-04-22 15:22:56 +0200

    added setup for connectionMongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 const port = 3000;
 
 // Parse JSON bodies
 app.use(express.json());
-
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
-app.use(express.static('public'));
+//Kan tas bort för vi ska använda vite.
+// app.use(express.static('public'));
 
-const bookRoutes = require('./routes/bookRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
+//Vad är detta för Routes? -Behövs dom? -Nej, skapa egna, ta bort detta sen!
+    const bookRoutes = require('./routes/bookRoutes');
+    const categoryRoutes = require('./routes/categoryRoutes');
 
-app.use(bookRoutes);
-app.use(categoryRoutes);
+    app.use(bookRoutes);
+    app.use(categoryRoutes);
+// till hit?
+
+//Sätt upp egna routes till våra databaser.
+
+//Vi kallar på vår MongoDB (Vi får tillgång till uppkopplingen)
+const connectionMongoDB = require('./connectionMongoDB')
+connectionMongoDB()
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
