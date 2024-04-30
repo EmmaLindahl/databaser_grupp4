f7e2bb5c340e946317410c878d4c1724dfb171b8
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-04-30 21:05:28 +0200

    changed GET for MongoDB

--- backend/controllers/plantControllerMongoDB.js
+++ backend/controllers/plantControllerMongoDB.js
@@ -3,7 +3,8 @@ const Plants = require ('../models/plant')
 exports.getHabitat = ( async (req, res) => {
     try {
         const allHabitats = await Plants.find()
-        return res.status(200).json(allHabitats.map(x => x.habitat))
+        return res.status(200).json(allHabitats)
+        // return res.status(200).json(allHabitats.map(x => x.habitat))
         
     } catch (error) {
         return res.status(500).json({error: error.message})
