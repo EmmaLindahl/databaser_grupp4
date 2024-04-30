f30dfdc6649425b25c3bd7bb21c8947a8cf54242
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-04-30 15:17:00 +0200

    changed base code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-// import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
-<!-- <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+<template>
+  <!-- <header>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
@@ -15,10 +12,10 @@
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
   <RouterView />
-</template> -->
+</template>
 
 <style scoped>
 /* header {
