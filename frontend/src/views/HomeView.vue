8fd360708ea8b34799f939e4c7fa57693a85f499
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-04-30 14:42:44 +0200

    created fetches for sql & Mongo

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,5 @@
-<script setup>
+<!-- Orginalkod: -->
+<!-- <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
@@ -6,4 +7,53 @@ import TheWelcome from '../components/TheWelcome.vue'
   <main>
     <TheWelcome />
   </main>
+</template> -->
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const loading = ref(true);
+const postData = ref(null);
+const postData2 = ref(null)
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/plants');
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    postData.value = await response.json();
+    loading.value = false;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    loading.value = false;
+  }
+});
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/habitat');
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    postData2.value = await response.json();
+    loading.value = false;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    loading.value = false;
+  }
+});
+</script>
+
+<template>
+  <main>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <h1 v-for="item in postData">{{ item.PlantName }}</h1>
+      <h1 v-for="item in postData2">{{ item }}</h1>
+    </div>
+    <TheWelcome />
+  </main>
 </template>
+
+
